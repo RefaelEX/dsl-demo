19d5b277f677487f6584a92e4742f1c160312609
princeex87 <princeex87@gmail.com>
2019-08-09 17:41:02 +0300

    update

--- files/demo.groovy
+++ files/demo.groovy
@@ -1,20 +1,8 @@
-pipelineJob('Demo') {
-
-  displayName('Demo')
-
-  definition {
-       cpsScm {
-            scm {
-                git('https://github.com/RefaelEX/dsl-demo.git')
-                scriptPath('demo.Jenkinsfile')
-            }
+pipelineJob('example') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('files/demo.Jenkinsfile'))
+            sandbox()
         }
-  }
-
-  parameters {
-    booleanParam('force', false, 'Recreate the branches even if they already exists. WARNING: this will override existing branches.')
-    stringParam('sourceBranch', 'develop', 'What branch to create the new branch from? (e.g. develop)')
-    stringParam('targetBranch', '', 'Name of the new branch (e.g. release/154.17)')
-  }
-
+    }
 }

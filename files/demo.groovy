9e2a964c96b7a61dcc564e4e8b54ab7b3dc1bc05
princeex87 <princeex87@gmail.com>
2019-08-10 12:58:07 +0300

    update

--- files/demo.groovy
+++ files/demo.groovy
@@ -1,19 +1,20 @@
-folder('demo folder')
+folder('demo folder'){
 
-pipelineJob('example') {
-    
-	displayName('Demo')
-	
-	definition {
-        cps {
-            script(readFileFromWorkspace('files/demo.Jenkinsfile'))
-            sandbox(true)
-        }
-    }
-	
-	 parameters {
-		booleanParam('force', false, 'Recreate the branches even if they already exists. WARNING: this will override existing branches.')
-		stringParam('sourceBranch', 'develop', 'What branch to create the new branch from? (e.g. develop)')
-		stringParam('targetBranch', '', 'Name of the new branch (e.g. release/154.17)')
-  }
+	pipelineJob('example') {
+		
+		displayName('Demo')
+		
+		definition {
+			cps {
+				script(readFileFromWorkspace('files/demo.Jenkinsfile'))
+				sandbox(true)
+			}
+		}
+		
+		 parameters {
+			booleanParam('force', false, 'Recreate the branches even if they already exists. WARNING: this will override existing branches.')
+			stringParam('sourceBranch', 'develop', 'What branch to create the new branch from? (e.g. develop)')
+			stringParam('targetBranch', '', 'Name of the new branch (e.g. release/154.17)')
+	  }
+	}
 }
